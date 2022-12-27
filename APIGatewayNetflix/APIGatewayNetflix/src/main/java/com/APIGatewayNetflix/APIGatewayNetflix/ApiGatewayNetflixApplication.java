39d8ff109832025c3b443471a3b849748721637a
quani1808 <115769212+quani1808@users.noreply.github.com>
2022-12-27 12:46:05 +0530

    Add files via upload

---
+++ APIGatewayNetflix/APIGatewayNetflix/src/main/java/com/APIGatewayNetflix/APIGatewayNetflix/ApiGatewayNetflixApplication.java
@@ -0,0 +1,14 @@
+package com.APIGatewayNetflix.APIGatewayNetflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+public class ApiGatewayNetflixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayNetflixApplication.class, args);
+	}
+
+}
