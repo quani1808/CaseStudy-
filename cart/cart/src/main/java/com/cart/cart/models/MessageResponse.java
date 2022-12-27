c1b43385910f3c239e557387dce758262db84c91
quani1808 <115769212+quani1808@users.noreply.github.com>
2022-12-27 12:47:04 +0530

    Add files via upload

---
+++ cart/cart/src/main/java/com/cart/cart/models/MessageResponse.java
@@ -0,0 +1,27 @@
+package com.cart.cart.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+public class MessageResponse {
+
+	private String message;
+
+	public MessageResponse(String string) {
+
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+
+
+
+}
